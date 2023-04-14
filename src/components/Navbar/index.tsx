81bf3d07733454495386d4a02ca1ac66c53bf010
ayush-lal <ayush.lal@outlook.com>
2023-04-14 10:56:56 +1000

    upgrade to ts

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavbarHeader = () => {
